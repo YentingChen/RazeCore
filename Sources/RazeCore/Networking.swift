22cdb30f0f4a9c7df8a0e72ffeea6c22742e809f
Yenting Chen <yeonbo0620@gmail.com>
2022-01-05 20:29:48 +0100

    Refactored first Networking API to include dependency injection for easier testing

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,27 +7,51 @@
 
 import Foundation
 
-extension RazeCore {
-    public class Networking {
+protocol NetworkSession {
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) 
+}
+
+extension URLSession: NetworkSession {
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         
+        let task = dataTask(with: url) { data, _, error in
+            
+            completionHandler(data, error)
+        }
+        
+        task.resume()
+        
+    }
+    
+    
+}
+
+extension RazeCore {
+    
+    public class Networking {
         
         /// Responsible for handling all networking calls
         public class Manager {
             
             public init() {}
             
-            private let session = URLSession.shared
+            internal var session: NetworkSession = URLSession.shared
             
+            /// Calls to the live internet to retrieve Data from a specific location
+            /// - Parameters:
+            ///   - url: The location you wish to fetch data from
+            ///   - completionHandler: Returns a result object which signifies the status of the request
             public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
                 
-                let task = session.dataTask(with: url) { data, response, error in
+                session.get(from: url) { data, error in
                     
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     
                     completionHandler(result)
                 }
                 
-                task.resume()
                 
             }
             
@@ -36,9 +60,12 @@ extension RazeCore {
     }
     
     public enum NetworkResult<T> {
+        
         case success(T)
+        
         case failure(Error?)
         
     }
+    
 }
 
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -8,20 +8,36 @@
 import XCTest
 @testable import RazeCore
 
+class NetworkSessionMock: NetworkSession {
+    
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        
+        completionHandler(data, error)
+        
+    }
+
+}
+
 final class RazeNetworkingTests: XCTestCase {
     
     func testLoadDataCall() {
         let manager = RazeCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
+        let data = Data([0,1,0,1])
         let expectation = XCTestExpectation(description: "Call for data")
-        guard let url = URL(string: "https://raywechlich.com") else {
-            return XCTFail("Could not create URL properly")
-        }
+        let url = URL(fileURLWithPath: "url")
+        session.data = data
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let returnData):
-                print(returnData)
-                XCTAssertNotNil(returnData, "Response data is nil")
+                
+                XCTAssertEqual(returnData, data, "manager returned unexpected data")
+                
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
